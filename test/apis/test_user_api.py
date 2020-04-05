09c9d008add290a07df6d1a698b407264f700df6
jet <qinjet@gmail.com>
2020-04-06 01:35:07 +0800

    add test case

--- test/apis/test_user_api.py
+++ test/apis/test_user_api.py
@@ -4,18 +4,11 @@ from service import UserService
 from unittest.mock import Mock
 
 
-# @pytest.fixture(scope="function")
-# def user_service() -> None:
-#     return Mock(spec=UserService)
-
-
-# @pytest.mark.usefixtures("client")
 class TestUserApi(unittest.TestCase):
 
     def setUp(self) -> None:
         self.service = Mock(spec=UserService)
 
-    # @pytest.fixture(autouse=True)
     @pytest.mark.usefixtures("client")
     def test_list_user(self, client):
         self.service.list_user.return_value = [{'id': 1, 'name': 'admin', 'email': 'admin@example.com'}]
